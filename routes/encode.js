547e4ade43b6ff08ba21b5cc6c71d4881adb5553
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-10-17 12:34:40 +0200

    encode route works now

--- routes/encode.js
+++ routes/encode.js
@@ -1,7 +1,9 @@
 /**
  * Module dependencies.
  */
+var fs = require('fs');
 var glitxt = require('glitxt');
+var request = require('request');
 var api = require('../lib/api');
 
 /**
@@ -10,28 +12,43 @@ var api = require('../lib/api');
  */
 module.exports = function(req, res, next) {
   var qText = req.query.text;
-  var qImg = req.query.img;
+  var qSource = req.query.source;
 
-  // if (qUrl) {
-  //   glitxt.encode.text(qUrl, function(data) {
-  //     // var tmp = api(model, {status: 'ok', message: data, source: qUrl});
-  //     // api.responseJson(res, tmp);
-  //     console.log('ENCODE');
-  //   });
-  // };
 
-  if (qImg !== undefined) {
-    console.log(qImage);
-    api.responseImage(res, process.env.PWD+'/cat.png');
+  // Check if a text query exist...
+  if (qText !== undefined) {
+    // If an image url is available, request the image and encode it.
+    if (qSource !== undefined) {
+      //console.log(qSource);
+      // If http:// is at the query, a slash is missing...
+      request.get({url: 'http://'+qSource, encoding: null}, function(error, response, data) {
+        //console.log(error);
+        var tmpBuffer = glitxt.encode(data, qText);
+        res.contentType = 'image/gif';
+        res.writeHead(200);
+        res.end(tmpBuffer);
+      });
+    }
+    // If no image is set, we use a default cat image...
+    else {
+      fs.readFile(__dirname+'/cat.gif', function(err, data) {
+        //console.log('ERROR: ', err);
+        var tmpBuffer = glitxt.encode(data, qText);
+        res.contentType = 'image/gif';
+        res.writeHead(200);
+        res.end(tmpBuffer);
+      });
+    }
   }
+  // If no text query can be found, send an error...
   else {
-    api.responseImage(res, process.env.PWD+'/cat2.png');
+    var obj = {
+      response: {
+        message: 'We need an text query to encode your message correct.'
+      }
+    };
+    res.send(api.model(res, obj));
   }
-  
-  //var tmpImage = glitxt.encode(qText, function(data) {
-    //var tmp = api.model(res, {status:'OK',response:{img:data}});
-    //api.responseJson(res, tmp);
-    api.responseImage(res, process.env.PWD+'/cat.gif');
-  //});
+
   log.info('GET /encode');
 };
