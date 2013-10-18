bccfb7ff067671b7536b4f9457ff789be16d72ba
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-10-18 09:48:07 +0200

    Added api description to ping.js

--- routes/ping.js
+++ routes/ping.js
@@ -3,8 +3,30 @@
  */
 var api = require('../lib/api');
 
+
 /**
- * The /ping route.
+ * @api {get} /ping Ping the API
+ * @apiVersion 0.1.0
+ * @apiName GetPing
+ * @apiGroup Ping
+ * @apiDescription
+ *     Send a ping request to check if the glitxt API is still working.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "meta": {
+ *         "code": 200,
+ *         "version": "0.1.0",
+ *         "url": "http://api.glitxt.com"
+ *       },
+ *       "response": {
+ *         "message": "pong"
+ *       }
+ *     }
+ *
+ * @apiExample Example usage:
+ *     curl -i http://api.glitxt.com/ping
  */
 module.exports = function(req, res, next) {
   var obj = {status:'OK', response:{message:'pong'}};
